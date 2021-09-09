bbe1b4b71a503a1e3eae2ea7091b38e3d46d5bc3
rachit-singh123 <79090124+rachit-singh123@users.noreply.github.com>
2021-09-09 17:43:30 +0530

    Add files via upload

---
+++ Quiz.js
@@ -0,0 +1,60 @@
+class Quiz {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play(){
+  
+   question.hide()
+   background("yellow");
+  fill(0);
+  textSize(30)
+  text("Result of the quiz",340,50);
+  Contestant.getPlayerInfo();
+   if(allContestants !== undefined){
+     Fill("Blue");
+     textSize(20);
+     text("NOTE: CONTESTANT WHO ANSWERED CORRECT ARE HIGHLIGHTED IN GREEN COLOR!",130,230);
+    Fill("Blue");
+    textSize(20);
+    text("NOTE: CONTESTANT WHO ANSWERED CORRECT ARE HIGHLIGHTED IN GREEN COLOR!",130,230);
+    for(var plr in allContestants){
+      debugger;
+      var correctAns = "2";
+      if(correctAns === allContestants[plr].answer)
+      fill("Green")
+    else
+     fill("red");
+     display_Answers+=30;
+     textSize(20);
+     text(allContestants[plr].name + ": "+ allContestants[plr].answer, 250,display_Answers)
+    }
+    
+  }
+  }
+}
